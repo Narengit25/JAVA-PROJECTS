5db0f94dce696567ae1e14def37de629485f9c57
NAREN <naren06251999@gmail.com>
2021-11-02 20:47:45 +0530

    dmy commit

---
+++ src/Datemonyear/java/dateFromNow.java
@@ -0,0 +1,21 @@
+package Datemonyear.java;
+
+public class dateFromNow {
+
+    public static void main(String[] args) {
+
+//    Date = 2/07/2021 print 2 months , 2 days after ;
+
+        int d = 2;
+        int m = 7;
+        int y = 2021;
+
+        d+=2;
+        m+=2; //2 months
+
+
+        System.out.println("2 months , 2 days after " + d + "/" + m + "/" + y);
+
+
+}
+}
