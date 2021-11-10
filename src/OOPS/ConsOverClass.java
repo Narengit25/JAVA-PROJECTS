4528ca53a7c67ebe4c85b398f4d06f6cba6632be
NAREN <naren06251999@gmail.com>
2021-11-10 19:17:01 +0530

    oops commit

---
+++ src/OOPS/ConsOverClass.java
@@ -0,0 +1,21 @@
+package OOPS;
+
+public class ConsOverClass {
+
+
+    public static void main(String[] args) {
+        ConsOverClass c = new ConsOverClass();
+        c.show();
+     }
+void get (){
+    System.out.print(" welcome");
+}
+void show(){
+
+    this.get();
+    System.out.println(" to nsic ");
+
+}
+
+}
+
